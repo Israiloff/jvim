85c947d5d6ef5c6760a7b20aa3f36552dccb8aac
Israiloff <stardustindark@gmail.com>
2024-06-22 11:14:46 +0000

    fixed: included modules' list;

--- lua/io/github/israiloff/init.lua
+++ lua/io/github/israiloff/init.lua
@@ -1,1 +1,5 @@
-print("io.github.israiloff")
+require("io.github.israiloff.config.lazy")
+require("io.github.israiloff.config.keymap")
+require("io.github.israiloff.config.formatting")
+require("io.github.israiloff.config.editor")
+require("io.github.israiloff.config.which-key")
