e8e91a11ecbaef0f58ca7c72019b8ce638e8bf7d
Israiloff <stardustindark@gmail.com>
2024-06-22 11:09:03 +0000

    added: plugins' list config;

---
+++ lua/io/github/israiloff/config/plugins.lua
@@ -0,0 +1,68 @@
+return {
+    {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+},
+{
+        "mfussenegger/nvim-jdtls",
+    },
+    {
+        "israiloff/darcula-java",
+        dependencies = "rktjmp/lush.nvim"
+    },
+    {
+        "petertriho/nvim-scrollbar",
+    },
+    {
+        "danymat/neogen",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+    },
+    {
+        "jackMort/ChatGPT.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "folke/trouble.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
+    },
+    {
+        "github/copilot.vim"
+    },
+    {
+        "dawsers/telescope-file-history.nvim"
+    },
+    {
+        "olimorris/persisted.nvim",
+        lazy = false,
+        config = true,
+    },
+    {
+        "Pocco81/auto-save.nvim"
+    },
+    {
+        "archibate/lualine-time"
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {}
+    }
+}
