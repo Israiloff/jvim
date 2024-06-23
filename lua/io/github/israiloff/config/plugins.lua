72c36064c839e92f201dc961c92391401aaac655
Israiloff <stardustindark@gmail.com>
2024-06-23 15:02:25 +0000

    added: null ls plugin; fixed: code structure;

--- lua/io/github/israiloff/config/plugins.lua
+++ lua/io/github/israiloff/config/plugins.lua
@@ -1,18 +1,17 @@
 return {
     {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
-},
-{
+        "nvim-lua/plenary.nvim"
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+                    vim.o.timeout = true
+                    vim.o.timeoutlen = 300
+                end,
+        opts = { }
+    },
+    {
         "mfussenegger/nvim-jdtls",
     },
     {
@@ -30,7 +29,6 @@ return {
         "jackMort/ChatGPT.nvim",
         dependencies = {
             "MunifTanjim/nui.nvim",
-            "nvim-lua/plenary.nvim",
             "folke/trouble.nvim",
             "nvim-telescope/telescope.nvim"
         }
@@ -62,14 +60,15 @@ return {
     },
     {
         "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
         opts = {}
     },
     {
         "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" }
     },
     {
         "neovim/nvim-lspconfig"
+    },
+    {
+        "nvimtools/none-ls.nvim",
     }
 }
