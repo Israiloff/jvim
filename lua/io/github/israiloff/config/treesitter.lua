fb1f22d524176118da275e1806cb53829e494615
Israiloff <stardustindark@gmail.com>
2024-06-23 18:34:20 +0000

    added: treesitter configs;

---
+++ lua/io/github/israiloff/config/treesitter.lua
@@ -0,0 +1,23 @@
+require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          "bash", "json", "lua", "java", "xml", "yaml"
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      })
