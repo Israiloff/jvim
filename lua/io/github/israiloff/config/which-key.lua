d116051e64633a84bea94175806a3f6fc35a46a1
Israiloff <stardustindark@gmail.com>
2024-06-23 14:26:24 +0000

    fixed: whichkey related mappings migration issue;

--- lua/io/github/israiloff/config/which-key.lua
+++ lua/io/github/israiloff/config/which-key.lua
@@ -4,4 +4,6 @@ local which_key = require("which-key")
 which_key.register({
     f = { "<cmd>Telescope find_files<cr>", "Find Files" },
     u = { "<cmd>so<cr>", "Update configs"},
+    c = { "<cmd>Ex<cr>", "Close buffer"},
+    q = { "<cmd>confirm q<CR>", "Quit"},
 }, { prefix = "<leader>", mode = "n" })
