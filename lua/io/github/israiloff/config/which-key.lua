ff8727a75032600d3b241691d82d2aa52478e209
Israiloff <stardustindark@gmail.com>
2024-06-23 15:42:50 +0000

    added: code actions related menu to whick key menu;

--- lua/io/github/israiloff/config/which-key.lua
+++ lua/io/github/israiloff/config/which-key.lua
@@ -6,4 +6,15 @@ which_key.register({
     u = { "<cmd>so<cr>", "Update configs"},
     c = { "<cmd>Ex<cr>", "Close buffer"},
     q = { "<cmd>confirm q<CR>", "Quit"},
+    l = {
+      name = "Code",
+      a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Action" },
+      d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+      w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+      f = { "<cmd>lua require('lvim.lsp.utils').format()<cr>", "Format" },
+      i = { "<cmd>LspInfo<cr>", "Info" },
+      j = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
+      k = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
+      r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+    },
 }, { prefix = "<leader>", mode = "n" })
