d60f0d7f1bb854d733f2e908a2adcd89f327c1da
Israiloff <stardustindark@gmail.com>
2024-06-23 15:45:36 +0000

    added: nvim cmp plugin support;

--- lua/io/github/israiloff/config/plugins.lua
+++ lua/io/github/israiloff/config/plugins.lua
@@ -70,5 +70,16 @@ return {
     },
     {
         "nvimtools/none-ls.nvim",
+    },
+    {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip"
+    },
     }
 }
