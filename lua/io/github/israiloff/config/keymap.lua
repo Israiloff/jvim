fb9bf5e7522763b0643c9094e5e878d897e58d42
Israiloff <stardustindark@gmail.com>
2024-06-25 11:48:03 +0000

    fixed: window navigation keymaps;

--- lua/io/github/israiloff/config/keymap.lua
+++ lua/io/github/israiloff/config/keymap.lua
@@ -1,1 +1,8 @@
+-- common
 vim.g.mapleader = " "
+
+-- window
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
