7705f026cf45cf22257bb665533ff5319a91ff99
Israiloff <stardustindark@gmail.com>
2024-06-25 12:14:25 +0000

    added: buffer change keymaps;

--- lua/io/github/israiloff/config/keymap.lua
+++ lua/io/github/israiloff/config/keymap.lua
@@ -6,3 +6,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- buffer
+vim.keymap.set("n", "<M-l>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<M-h>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
