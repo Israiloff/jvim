8abff061067be784a932d13ab09e047b360e525b
Israiloff <stardustindark@gmail.com>
2024-06-27 10:12:59 +0000

    fixed: lualine format;

--- lua/io/github/israiloff/config/lualine.lua
+++ lua/io/github/israiloff/config/lualine.lua
@@ -1,30 +1,54 @@
+---@diagnostic disable: param-type-mismatch
+local nvim_tree_shift = {
+	function()
+		len = vim.api.nvim_win_get_width(require("nvim-tree.view").get_winnr()) - 1
+		title = "Nvim-Tree"
+		left = (len - #title) / 2
+		right = len - left - #title
+		return string.rep(" ", left) .. title .. string.rep(" ", right)
+	end,
+	cond = require("nvim-tree.view").is_visible,
+	color = "Normal",
+}
+
 require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		component_separators = {
+			left = "",
+			right = "",
+		},
+		section_separators = {
+			left = "",
+			right = "",
+		},
+		disabled_filetypes = {
+			--"NvimTree",
+			"TelescopePrompt",
+			"packer",
+			"toggleterm",
+		},
+		always_divide_middle = true,
+		globalstatus = true,
+	},
 	sections = {
 		lualine_a = {
+			nvim_tree_shift,
 			"mode",
-			color = nil,
 		},
-		lualine_b = {
-			"branch",
-			"diff",
-			"diagnostics",
-			color = nil,
-		},
-		lualine_c = {
-			"filename",
-			color = nil,
-		},
-		lualine_x = {
-			"filetype",
-			color = nil,
-		},
-		lualine_y = {
-			"location",
-			color = nil,
-		},
-		lualine_z = {
-			"ctime",
-			color = nil,
-		},
+		lualine_b = { "branch" },
+		lualine_c = { "filename" },
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
 })
