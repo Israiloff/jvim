330744da7648092b9a9fee5ca68ba4fd2e6614f8
Israiloff <stardustindark@gmail.com>
2024-07-01 11:57:41 +0000

    fixed: folding logic;

--- lua/io/github/israiloff/config/nvim-ufo.lua
+++ lua/io/github/israiloff/config/nvim-ufo.lua
@@ -3,12 +3,18 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
--- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-require('ufo').setup({
-    provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
-    end
-})
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
+local language_servers = require("lspconfig").util.available_servers()
+for _, ls in ipairs(language_servers) do
+	require("lspconfig")[ls].setup({
+		capabilities = capabilities,
+	})
+end
+require("ufo").setup()
