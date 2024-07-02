98cb7b5f72ed68dfd042f839cf385250a974015b
Israiloff <stardustindark@gmail.com>
2024-07-02 13:57:54 +0000

    fixed: null ls settings;

--- lua/io/github/israiloff/config/none-ls.lua
+++ lua/io/github/israiloff/config/none-ls.lua
@@ -1,9 +1,30 @@
 local null_ls = require("null-ls")
-
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
-    },
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_args = function(params)
+				if params.filetype == "ruby" then
+					return { "--config", "--plugin=@prettier/plugin-ruby" }
+				end
+
+				return {
+					"--config",
+					"--arrow-parens",
+					"avoid",
+				}
+			end,
+			extra_filetypes = { "ruby" },
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		formatting.sql_formatter,
+		formatting.golines,
+		formatting.gofmt,
+		formatting.shfmt,
+		diagnostics.rubocop,
+		require("none-ls.diagnostics.eslint"),
+	},
 })
