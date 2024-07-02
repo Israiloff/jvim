2f071a0f13d61225cb061babf1d425ed8f2f4b49
Israiloff <stardustindark@gmail.com>
2024-07-02 05:45:18 +0000

    added: 'goto' mappings for lsp operations;

--- lua/io/github/israiloff/config/keymap.lua
+++ lua/io/github/israiloff/config/keymap.lua
@@ -10,3 +10,12 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 -- buffer
 vim.keymap.set("n", "<M-l>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<M-h>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+
+-- LSP mappings
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto references", expr = true, silent = true })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "<M-p>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+vim.keymap.set("n", "<M-i>", vim.lsp.buf.hover, { desc = "Show hover" })
