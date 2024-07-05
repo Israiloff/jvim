25db8604deec2e40765cde018dae88a87030481e
Israiloff <stardustindark@gmail.com>
2024-07-05 12:46:12 +0500

    removed: temporary darcula colors' fixer;

--- lua/io/github/israiloff/config/darcula-java.lua
+++
@@ -1,6 +0,0 @@
-local wk_status, _ = pcall(require, "which-key")
-
-if wk_status then
-	vim.api.nvim_set_hl(0, "WhichKeyFloat", { link = "FloatBorder" })
-	vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#CD7832" })
-end
