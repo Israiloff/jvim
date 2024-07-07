21e6862d85fc45127790e388a96ab04fe96047ec
Israiloff <stardustindark@gmail.com>
2024-07-07 21:05:13 +0500

    added: lazy options into the which-key menu;

--- lua/io/github/israiloff/config/which-key.lua
+++ lua/io/github/israiloff/config/which-key.lua
@@ -164,6 +164,17 @@ which_key.register({
 		"<Plug>(comment_toggle_linewise_current)",
 		"Comment current line",
 	},
+	p = {
+		name = "Plugins",
+		i = { "<cmd>Lazy install<cr>", "Install" },
+		s = { "<cmd>Lazy sync<cr>", "Sync" },
+		S = { "<cmd>Lazy clear<cr>", "Status" },
+		c = { "<cmd>Lazy clean<cr>", "Clean" },
+		u = { "<cmd>Lazy update<cr>", "Update" },
+		p = { "<cmd>Lazy profile<cr>", "Profile" },
+		l = { "<cmd>Lazy log<cr>", "Log" },
+		d = { "<cmd>Lazy debug<cr>", "Debug" },
+	},
 }, {
 	prefix = "<leader>",
 	mode = "n",
