674937d96b42a261fd32540dee6a599edd53da31
Israiloff <stardustindark@gmail.com>
2024-07-08 15:21:51 +0500

    fixed: folding feature related gui issues;

--- lua/io/github/israiloff/config/nvim-ufo.lua
+++ lua/io/github/israiloff/config/nvim-ufo.lua
@@ -1,10 +1,11 @@
-vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldcolumn = "0"
+vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+local ufo = require("ufo")
+vim.keymap.set("n", "zR", ufo.openAllFolds)
+vim.keymap.set("n", "zM", ufo.closeAllFolds)
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
