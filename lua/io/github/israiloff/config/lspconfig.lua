1d9d2a249b599f0cd34e50c8dc6d93a74f1ef730
Israiloff <stardustindark@gmail.com>
2024-07-10 12:40:12 +0500

    fixed: java related lsp configs;

--- lua/io/github/israiloff/config/lspconfig.lua
+++ lua/io/github/israiloff/config/lspconfig.lua
@@ -10,10 +10,4 @@ lspconfig.lua_ls.setup({
 	},
 })
 
-lspconfig.jdtls.setup({
-	jdtls = function()
-		require("java").setup({})
-	end,
-})
-
 require("lspconfig.ui.windows").default_options.border = "single"
