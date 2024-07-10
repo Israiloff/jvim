59a56e9d04a90b1812e44ce86b65c813818ebbb6
Israiloff <stardustindark@gmail.com>
2024-07-10 12:44:47 +0500

    fixed: dap configs;

--- lua/io/github/israiloff/config/nvim-dap.lua
+++ lua/io/github/israiloff/config/nvim-dap.lua
@@ -1,1 +1,26 @@
-require("dapui").setup()
+local dap = require("dap")
+
+dap.adapters.java = {
+	type = "executable",
+	command = "java",
+	args = {
+		"-jar",
+		vim.fn.stdpath("data")
+			.. "/mason/packages/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
+	},
+}
+
+dap.configurations.java = {
+	{
+		type = "java",
+		request = "launch",
+		name = "Launch Java",
+		mainClass = function()
+			return vim.fn.input("Path to main class > ", vim.fn.getcwd() .. "/", "file")
+		end,
+		projectName = function()
+			return vim.fn.input("Path to project > ", vim.fn.getcwd(), "file")
+		end,
+		cwd = "${workspaceFolder}",
+	},
+}
