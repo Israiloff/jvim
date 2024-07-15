d0326670c1d565d6187afc53189309592cc1f3a5
Israiloff <stardustindark@gmail.com>
2024-07-15 15:56:51 +0500

    fixed: nvim-navic initialization logic;

--- lua/io/github/israiloff/config/nvim-navic.lua
+++ lua/io/github/israiloff/config/nvim-navic.lua
@@ -28,8 +28,6 @@ if not utils_status then
 	return
 end
 
--- vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-
 local winbar_filetype_exclude = {
 	"help",
 	"startify",
@@ -204,49 +202,51 @@ local function create_winbar()
 	})
 end
 
-create_winbar()
+function M.setup()
+	create_winbar()
 
-navic.setup({
-	icons = {
-		Array = icons.kind.Array .. " ",
-		Boolean = icons.kind.Boolean .. " ",
-		Class = icons.kind.Class .. " ",
-		Color = icons.kind.Color .. " ",
-		Constant = icons.kind.Constant .. " ",
-		Constructor = icons.kind.Constructor .. " ",
-		Enum = icons.kind.Enum .. " ",
-		Enummber = icons.kind.EnumMember .. " ",
-		Event = icons.kind.Event .. " ",
-		Field = icons.kind.Field .. " ",
-		File = icons.kind.File .. " ",
-		Folder = icons.kind.Folder .. " ",
-		Function = icons.kind.Function .. " ",
-		Interface = icons.kind.Interface .. " ",
-		Key = icons.kind.Key .. " ",
-		Keyword = icons.kind.Keyword .. " ",
-		thod = icons.kind.Method .. " ",
-		dule = icons.kind.Module .. " ",
-		Namespace = icons.kind.Namespace .. " ",
-		Null = icons.kind.Null .. " ",
-		Number = icons.kind.Number .. " ",
-		Object = icons.kind.Object .. " ",
-		Operator = icons.kind.Operator .. " ",
-		Package = icons.kind.Package .. " ",
-		Property = icons.kind.Property .. " ",
-		Reference = icons.kind.Reference .. " ",
-		Snippet = icons.kind.Snippet .. " ",
-		String = icons.kind.String .. " ",
-		Struct = icons.kind.Struct .. " ",
-		Text = icons.kind.Text .. " ",
-		TypeParameter = icons.kind.TypeParameter .. " ",
-		Unit = icons.kind.Unit .. " ",
-		Value = icons.kind.Value .. " ",
-		Variable = icons.kind.Variable .. " ",
-	},
-	highlight = true,
-	separator = " " .. icons.ui.ChevronRight .. " ",
-	depth_limit = 0,
-	depth_limit_indicator = "..",
-})
+	navic.setup({
+		icons = {
+			Array = icons.kind.Array .. " ",
+			Boolean = icons.kind.Boolean .. " ",
+			Class = icons.kind.Class .. " ",
+			Color = icons.kind.Color .. " ",
+			Constant = icons.kind.Constant .. " ",
+			Constructor = icons.kind.Constructor .. " ",
+			Enum = icons.kind.Enum .. " ",
+			Enummber = icons.kind.EnumMember .. " ",
+			Event = icons.kind.Event .. " ",
+			Field = icons.kind.Field .. " ",
+			File = icons.kind.File .. " ",
+			Folder = icons.kind.Folder .. " ",
+			Function = icons.kind.Function .. " ",
+			Interface = icons.kind.Interface .. " ",
+			Key = icons.kind.Key .. " ",
+			Keyword = icons.kind.Keyword .. " ",
+			thod = icons.kind.Method .. " ",
+			dule = icons.kind.Module .. " ",
+			Namespace = icons.kind.Namespace .. " ",
+			Null = icons.kind.Null .. " ",
+			Number = icons.kind.Number .. " ",
+			Object = icons.kind.Object .. " ",
+			Operator = icons.kind.Operator .. " ",
+			Package = icons.kind.Package .. " ",
+			Property = icons.kind.Property .. " ",
+			Reference = icons.kind.Reference .. " ",
+			Snippet = icons.kind.Snippet .. " ",
+			String = icons.kind.String .. " ",
+			Struct = icons.kind.Struct .. " ",
+			Text = icons.kind.Text .. " ",
+			TypeParameter = icons.kind.TypeParameter .. " ",
+			Unit = icons.kind.Unit .. " ",
+			Value = icons.kind.Value .. " ",
+			Variable = icons.kind.Variable .. " ",
+		},
+		highlight = true,
+		separator = " " .. icons.ui.ChevronRight .. " ",
+		depth_limit = 0,
+		depth_limit_indicator = "..",
+	})
+end
 
 return M
