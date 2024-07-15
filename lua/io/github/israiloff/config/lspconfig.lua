38266feb5d5b309d3007f4181370809da03b5dfb
Israiloff <stardustindark@gmail.com>
2024-07-15 16:28:59 +0500

    fixed: lsp setup;

--- lua/io/github/israiloff/config/lspconfig.lua
+++ lua/io/github/israiloff/config/lspconfig.lua
@@ -1,4 +1,20 @@
-local lspconfig = require("lspconfig")
+local log_status, log = pcall(require, "io.github.israiloff.config.logger")
+
+if not log_status then
+	print("Error: 'io.github.israiloff.config.logger' not found. LSP will not be configured.")
+	return
+end
+
+local logger_name = "io.github.israiloff.config.lspconfig"
+
+local lsp_status, lspconfig = pcall(require, "lspconfig")
+
+if not lsp_status then
+	log.error(logger_name, "'lspconfig' not found. LSP will not be configured.")
+	return
+end
+
+log.debug(logger_name, "Configuring LSP")
 
 lspconfig.lua_ls.setup({
 	settings = {
