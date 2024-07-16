32299b5b63a4379e4fddcec467062617935c1735
Israiloff <stardustindark@gmail.com>
2024-07-16 12:00:48 +0500

    added: markdown support;

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+require("lvim.lsp.manager").setup("marksman", {
+	root_dir = require("lspconfig").util.root_pattern(".git", ".marksman.toml", "package.json", "pom.xml", ".settings"),
+	filetypes = {
+		"markdown",
+		"markdown.mdx",
+	},
+})
--- lua/io/github/israiloff/config/plugins.lua
+++ lua/io/github/israiloff/config/plugins.lua
@@ -174,4 +174,12 @@ return {
 	{
 		"SmiteshP/nvim-navic",
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
