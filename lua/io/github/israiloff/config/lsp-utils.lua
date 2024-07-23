e076170f04a3c718e338d0e80d51216986e9edb6
Israiloff <stardustindark@gmail.com>
2024-07-23 13:15:14 +0500

    added: lsp auto install feature;

---
+++ lua/io/github/israiloff/config/lsp-utils.lua
@@ -0,0 +1,76 @@
+local log_status, log = pcall(require, "io.github.israiloff.config.logger")
+
+if not log_status then
+	print("Error: 'io.github.israiloff.config.logger' not found. LSP will not be configured.")
+	return
+end
+
+local logger_name = "io.github.israiloff.config.lsp-utils"
+
+Utils = {}
+
+local reg_status, registry = pcall(require, "mason-registry")
+local ml_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+
+function Utils.install_package(package_name)
+	log.info(logger_name, "install_package started for package '" .. package_name .. "'.")
+
+	if not reg_status then
+		log.error(logger_name, "'mason-registry' not found. Aborting install_package.")
+		return false
+	end
+
+	if not registry.has_package(package_name) then
+		vim.notify("Package " .. package_name .. " is not available in the Mason registry.", vim.log.levels.ERROR)
+		return false
+	end
+
+	local pkg = registry.get_package(package_name)
+
+	if pkg.is_installed(pkg) then
+		log.warn(logger_name, "Package " .. package_name .. " is already installed.")
+		return true
+	end
+
+	log.info(logger_name, "Installing package '" .. package_name .. "'...")
+
+	local install_status, _ = pcall(function()
+		pkg.install(pkg)
+	end)
+
+	log.info(logger_name, "install_package finished for package '" .. package_name .. "'.")
+	return install_status
+end
+
+function Utils.get_available_servers(filter)
+	if not reg_status then
+		log.error(logger_name, "'mason-registry' not found. Aborting get_supported_servers.")
+		return
+	end
+
+	if not ml_status then
+		log.error(logger_name, "'mason-lspconfig' not found. Aborting get_supported_servers.")
+		return
+	end
+
+	registry.refresh()
+
+	local supported_servers = mason_lspconfig.get_available_servers(filter)
+	return supported_servers or {}
+end
+
+function Utils.already_installed(available)
+	if not reg_status then
+		log.error(logger_name, "'mason-registry' not found. Aborting already_installed.")
+		return false
+	end
+
+	for _, server in ipairs(available) do
+		if registry.is_installed(server) then
+			return true
+		end
+	end
+	return false
+end
+
+return Utils
--- lua/io/github/israiloff/config/mason-lspconfig.lua
+++ lua/io/github/israiloff/config/mason-lspconfig.lua
@@ -14,6 +14,8 @@ if not lsp_status then
 	return
 end
 
+log.info(logger_name, "Setting up Mason LSPConfig")
+
 mason_lspconfig.setup({
 	ensure_installed = {
 		"lua_ls",
@@ -25,4 +27,58 @@ mason_lspconfig.setup({
 			"lemminx",
 		},
 	},
+	handlers = {
+		function(server_name)
+			require("lspconfig")[server_name].setup({
+				on_attach = function(client, bufnr)
+					vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+					local navic_status, navic = pcall(require, "nvim-navic")
+					if navic_status then
+						navic.attach(client, bufnr)
+					end
+				end,
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				flags = { debounce_text_changes = 200 },
+			})
+		end,
+	},
+})
+
+local utils_status, utils = pcall(require, "io.github.israiloff.config.lsp-utils")
+
+if not utils_status then
+	log.error(
+		logger_name,
+		"'io.github.israiloff.config.utils' not found. Filetype auto resolve will not be configured."
+	)
+	return
+end
+
+log.info(logger_name, "Setting up Mason LSPConfig filetype auto resolve")
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local filetype = vim.bo.filetype
+		local availables = utils.get_available_servers({ filetype = filetype })
+
+		log.info(logger_name, "Available servers for '" .. filetype .. "' : " .. vim.inspect(availables))
+
+		if #availables == 0 or availables == nil then
+			log.warn(logger_name, "No available servers for " .. filetype)
+			return
+		end
+
+		if utils.already_installed(availables) then
+			log.info(logger_name, "Server for '" .. filetype .. "' already installed")
+			return
+		end
+
+		for _, server in ipairs(availables) do
+			log.info(logger_name, "Server: " .. vim.inspect(server))
+			if Utils.install_package(server) then
+				log.info(logger_name, "Server for '" .. filetype .. "' installed successfully")
+				break
+			end
+		end
+	end,
 })
