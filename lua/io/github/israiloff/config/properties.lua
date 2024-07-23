64a2381db47a3bda44f8bbb1f1cb2ed3c630329b
Israiloff <stardustindark@gmail.com>
2024-07-23 15:05:29 +0500

    fixed: default property values;

--- lua/io/github/israiloff/config/properties.lua
+++ lua/io/github/israiloff/config/properties.lua
@@ -1,14 +1,14 @@
 return {
 	version = "0.0.1",
 	logger = {
-		enabled = true,
+		enabled = false,
 		level = {
 			debug = true,
 			info = true,
 			warn = true,
 			error = true,
 		},
-		enabled_loggers = { "io.github.israiloff.config.mason-lspconfig" },
+		enabled_loggers = { "*" },
 	},
 	gui = {
 		transparent = true,
