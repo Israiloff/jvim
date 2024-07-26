e02dcd7b4431b74f477b3e4a430401fe8c18e4a9
Israiloff <stardustindark@gmail.com>
2024-07-26 12:03:54 +0500

    added: lsp client info in status line;

--- lua/io/github/israiloff/config/lualine.lua
+++ lua/io/github/israiloff/config/lualine.lua
@@ -1,4 +1,6 @@
 local icons = require("io.github.israiloff.config.icons")
+local null_ls = require("null-ls")
+local null_ls_sources = require("null-ls.sources")
 
 ---@diagnostic disable: param-type-mismatch
 local nvim_tree_shift = {
@@ -12,6 +14,28 @@ local nvim_tree_shift = {
 	cond = require("nvim-tree.view").is_visible,
 	color = "Normal",
 }
+
+local function get_providers(filetype)
+	local available_sources = null_ls_sources.get_available(filetype)
+	local registered = {}
+	for _, source in ipairs(available_sources) do
+		for method in pairs(source.methods) do
+			registered[method] = registered[method] or {}
+			table.insert(registered[method], source.name)
+		end
+	end
+	return registered
+end
+
+local function get_registered_linters(filetype)
+	return vim.tbl_flatten(vim.tbl_map(function(m)
+		return get_providers(filetype)[m] or {}
+	end, {
+		null_ls.methods.DIAGNOSTICS,
+		null_ls.methods.DIAGNOSTICS_ON_OPEN,
+		null_ls.methods.DIAGNOSTICS_ON_SAVE,
+	}))
+end
 
 require("lualine").setup({
 	options = {
@@ -73,7 +97,6 @@ require("lualine").setup({
 			},
 		},
 		lualine_x = {
-			"filename",
 			{
 				"diagnostics",
 				sources = { "nvim_diagnostic" },
@@ -83,6 +106,44 @@ require("lualine").setup({
 					info = icons.diagnostics.BoldInformation .. " ",
 					hint = icons.diagnostics.BoldHint .. " ",
 				},
+			},
+			{
+				function()
+					local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+					if #buf_clients == 0 then
+						return "LSP Inactive"
+					end
+
+					local buf_ft = vim.bo.filetype
+					local buf_client_names = {}
+					local copilot_active = false
+
+					for _, client in pairs(buf_clients) do
+						if client.name ~= "null-ls" and client.name ~= "GitHub Copilot" then
+							table.insert(buf_client_names, client.name)
+						end
+
+						if client.name == "GitHub Copilot" then
+							copilot_active = true
+						end
+					end
+
+					vim.list_extend(buf_client_names, get_providers(buf_ft)[null_ls.methods.FORMATTING] or {})
+					vim.list_extend(buf_client_names, get_registered_linters(buf_ft))
+
+					local unique_client_names = table.concat(buf_client_names, ", ")
+					local language_servers = string.format("[%s]", unique_client_names)
+
+					if copilot_active then
+						language_servers = language_servers .. "%#SLCopilot#" .. " " .. icons.ui.Copilot .. "%*"
+					end
+
+					return language_servers
+				end,
+				color = { gui = "bold" },
+				cond = function()
+					return vim.o.columns > 100
+				end,
 			},
 			"encoding",
 			"filetype",
