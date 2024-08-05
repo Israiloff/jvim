355aef249f83cf25f191c1fc7798cb02213236fd
Israiloff <stardustindark@gmail.com>
2024-08-05 17:18:32 +0500

    removed: markdown command;

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,7 +2,6 @@ local lspconfig = require("lspconfig")
 
 lspconfig.marksman.setup({
 	root_dir = lspconfig.util.root_pattern(".git", ".marksman.toml", "package.json", "pom.xml", ".settings"),
-    cmd = { "/root/.local/share/nvim/mason/bin/marksman", "server" },
 	filetypes = {
 		"markdown",
 		"markdown.mdx",
