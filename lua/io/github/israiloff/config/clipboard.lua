058863f3bdfb1a6e1b557d280d779b6a09bfee0d
Israiloff <stardustindark@gmail.com>
2024-10-24 11:05:52 +0500

    added: clipboard configs;

---
+++ lua/io/github/israiloff/config/clipboard.lua
@@ -0,0 +1,14 @@
+vim.g.clipboard = {
+    name = "cb",
+    copy = {
+        ["+"] = "cb copy",
+        ["*"] = "cb copy",
+    },
+    paste = {
+        ["+"] = "cb paste",
+        ["*"] = "cb paste",
+    },
+    cache_enabled = 0,
+}
+
+vim.opt.clipboard:append("unnamedplus")
