f3463a67145f3fc0e9213ad6dc4d9d255304343e
Israiloff <stardustindark@gmail.com>
2025-08-08 10:59:49 +0500

    added: os resolve process logging;

--- lua/io/github/israiloff/config/os.lua
+++ lua/io/github/israiloff/config/os.lua
@@ -1,3 +1,12 @@
+local logger_status, logger = pcall(require, "io.github.israiloff.config.logger")
+
+if not logger_status then
+    print("JAVA: io.github.israiloff.config.logger not found, please install it and try again")
+    return
+end
+
+local logger_name = "io.github.israiloff.config.os"
+
 OS = {
     WINDOWS = "win",
     LINUX = "linux",
@@ -5,15 +14,23 @@ OS = {
 }
 
 function OS.get_current_os()
+    logger.debug(logger_name, "OS: determining current operating system...")
+
     local os_real_name = vim.fn.expand("$OS")
 
+    logger.debug(logger_name, "OS: real name is '" .. os_real_name .. "'")
+
     if string.match(os_real_name, "[Mm][Aa][Cc]") then
+        logger.debug(logger_name, "OS: detected macOS")
         return OS.MAC
     elseif string.match(os_real_name, "[Ww][Ii][Nn]") then
+        logger.debug(logger_name, "OS: detected Windows")
         return OS.WINDOWS
     elseif string.match(os_real_name, "[Ll][Ii][Nn][Uu][Xx]") then
+        logger.debug(logger_name, "OS: detected Linux")
         return OS.LINUX
     else
+        logger.error(logger_name, "OS: unknown operating system '" .. os_real_name .. "'")
         return nil
     end
 end
