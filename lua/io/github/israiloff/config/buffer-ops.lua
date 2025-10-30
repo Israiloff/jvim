193e4796cb9b15000b2016eece3185f18d64d389
Israiloff <stardustindark@gmail.com>
2025-10-30 15:57:21 +0500

    fixed: current buffer closing issues;

--- lua/io/github/israiloff/config/buffer-ops.lua
+++ lua/io/github/israiloff/config/buffer-ops.lua
@@ -7,19 +7,51 @@ vim.api.nvim_create_user_command("CmCloseAllBuffers", function()
     end
 end, {})
 
-vim.api.nvim_create_user_command("CmCloseCurrentBuffer", function()
-    local current_buf = vim.api.nvim_get_current_buf()
-    local buffers = vim.tbl_filter(function(buf)
-        return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.bo[buf].filetype ~= "NvimTree"
-    end, vim.api.nvim_list_bufs())
+local function is_normal_listed(b, cur)
+    return vim.api.nvim_buf_is_valid(b)
+        and vim.api.nvim_buf_is_loaded(b)
+        and vim.bo[b].buflisted
+        and vim.bo[b].filetype ~= "NvimTree"
+        and vim.bo[b].buftype == ""
+        and b ~= cur
+end
 
-    if #buffers > 1 then
-        for _, buf in ipairs(buffers) do
-            if buf ~= current_buf then
-                vim.cmd("buffer " .. buf)
-                vim.cmd("bdelete " .. current_buf)
+local function is_listed_any(b, cur)
+    return vim.api.nvim_buf_is_valid(b) and vim.bo[b].buflisted and vim.bo[b].filetype ~= "NvimTree" and b ~= cur
+end
+
+vim.api.nvim_create_user_command("CmCloseCurrentBuffer", function(opts)
+    local force = opts.bang
+    local cur = vim.api.nvim_get_current_buf()
+
+    local bufs = vim.api.nvim_list_bufs()
+    local target = nil
+
+    for _, b in ipairs(bufs) do
+        if is_normal_listed(b, cur) then
+            target = b
+            break
+        end
+    end
+
+    if not target then
+        for _, b in ipairs(bufs) do
+            if is_listed_any(b, cur) then
+                target = b
                 break
             end
         end
     end
-end, {})
+
+    if not target then
+        vim.cmd("enew")
+        target = vim.api.nvim_get_current_buf()
+    else
+        vim.cmd("buffer " .. target)
+    end
+
+    local bt = vim.bo[cur].buftype
+    local del = (bt == "" or bt == "acwrite") and "bdelete" or "bwipeout"
+    local cmd = del .. (force and "! " or " ") .. cur
+    pcall(vim.api.nvim_command, cmd)
+end, { bang = true })
