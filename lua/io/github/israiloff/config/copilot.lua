b95f432a16850818b33612d24cdd2f3aa139a523
Israiloff <stardustindark@gmail.com>
2025-10-30 17:32:02 +0500

    fixed: <Tab> autofill disabling for copilot issue;

--- lua/io/github/israiloff/config/copilot.lua
+++ lua/io/github/israiloff/config/copilot.lua
@@ -1,11 +1,13 @@
+vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
+
 vim.api.nvim_set_keymap("i", "<M-l>", 'copilot#Accept("<CR>")', {
-	silent = true,
-	expr = true,
+    silent = true,
+    expr = true,
 })
 
 -- Keymap for MAC OS (Option + l)
 vim.api.nvim_set_keymap("i", "¬", 'copilot#Accept("<CR>")', {
-	silent = true,
-	expr = true,
+    silent = true,
+    expr = true,
 })
