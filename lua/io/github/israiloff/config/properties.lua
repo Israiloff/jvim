b0b44bf364773302eca5165773f5803a0ac8ed9e
Israiloff <stardustindark@gmail.com>
2025-10-30 17:32:42 +0500

    modified: version to 0.23.46;

--- lua/io/github/israiloff/config/properties.lua
+++ lua/io/github/israiloff/config/properties.lua
@@ -1,6 +1,6 @@
 return {
     -- Version of the JVIM IDE.
-    version = "0.23.45",
+    version = "0.23.46",
     -- Logging configuration.
     -- This is used to determine the logging level and whether logging is enabled.
     logger = {
