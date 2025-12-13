4898b62d2e28d53362582ae9431b491260c944fc
Israiloff <stardustindark@gmail.com>
2025-12-14 04:09:46 +0500

    fixed: global clipboard issue (for remote servers accessed by ssh); removed: cb clipboard logic;

--- lua/io/github/israiloff/config/clipboard.lua
+++ lua/io/github/israiloff/config/clipboard.lua
@@ -1,31 +1,11 @@
-if vim.fn.executable("win32yank") == 1 then
-    -- win32yank is available, use it for clipboard
-    vim.g.clipboard = {
-        name = "win32yank",
-        copy = {
-            ["+"] = "win32yank.exe -i --crlf",
-            ["*"] = "win32yank.exe -i --crlf",
-        },
-        paste = {
-            ["+"] = "win32yank.exe -o --lf",
-            ["*"] = "win32yank.exe -o --lf",
-        },
-        cache_enabled = 0,
-    }
-elseif vim.fn.executable("cb") == 1 then
-    -- cb is available, use it for clipboard
-    vim.g.clipboard = {
-        name = "cb_clipboard",
-        copy = {
-            ["+"] = "cb",
-            ["*"] = "cb",
-        },
-        paste = {
-            ["+"] = "cb -o",
-            ["*"] = "cb -o",
-        },
-        cache_enabled = 0,
-    }
-end
-
-vim.opt.clipboard:append("unnamedplus")
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
